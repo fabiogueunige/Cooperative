b3d22c161e2808166b13fe61d573114af72171e3
fabiogueunige <s5004782@studenti.unige.it>
2024-11-07 15:53:06 +0100

    starting commit

--- SolutionEx1/ComputeActivationFunctions.m
+++
@@ -1,9 +0,0 @@
-function [uvms] = ComputeActivationFunctions(uvms, mission)
-% compute the activation functions here
-
-% arm tool position control
-% always active
-uvms.A.t = eye(6);
-
-% vehicle position task
-uvms.A.gv = eye(3);
