1e44d52bf8e64ea3ebe7108df1a29e240f013411
fabiogueunige <s5004782@studenti.unige.it>
2024-11-14 21:48:10 +0100

    Ex3 start

--- SolutionEx1/matlab_scripts/UpdateMissionPhase.m
+++
@@ -1,7 +0,0 @@
-function [uvms, mission] = UpdateMissionPhase(uvms, mission)
-    switch mission.phase
-        case 1  
-            % add policy for changing phase
-    end
-end
-
--- matlab_Starting_scripts/ComputeActivationFunctions.m
+++
@@ -1,6 +0,0 @@
-function [uvms] = ComputeActivationFunctions(uvms, mission)
-% compute the activation functions here
-
-% arm tool position control
-% always active
-uvms.A.t = eye(6);
